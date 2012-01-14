c633188cb4b124a610f884271f7c49542634b95b
TaylorEisman12 <taylor@theeismans.com>
2012-01-13 20:43:18 -0600

    Started project created variables

--- Eisman_Taylor_01:12.js
+++ Eisman_Taylor_01:12.js
@@ -8,18 +8,9 @@ var worker = "Henry",
     maxInventory = 60,
     suckers = 15,
     gum = 5,
+    gummyBears = 5
     restock = true
 ;
 
-var jollyRanchers = [ "Cherry", 10, "Grape", 2, "Apple", 8 ];
-
-var outputJob = function () {
-	console.log( worker + " you need to restock the candy." );
-	console.log("We need " + maxInventory + " total pieces of candy on the shelf.");
-};
-
-if ( restock == true ){
-	outputJob() ;
-
-};
+var jollyRanchers = [ "Cherry", 5, "Grape", 2, "Apple", 8 ];
 
