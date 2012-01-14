92d9b546519e376242f602dfe651937d198ef637
TaylorEisman12 <taylor@theeismans.com>
2012-01-13 20:45:20 -0600

    creating functions

--- Eisman_Taylor_01:12.js
+++ Eisman_Taylor_01:12.js
@@ -14,3 +14,15 @@ var worker = "Henry",
 
 var jollyRanchers = [ "Cherry", 5, "Grape", 2, "Apple", 8 ];
 
+var outputJob = function () {
+    console.log( worker + " you need to restock the candy." );
+	console.log("We need " + maxInventory + " total pieces of candy on the shelf.");
+};
+
+if ( restock == true ){
+	outputJob() ;
+
+};
+
+
+
