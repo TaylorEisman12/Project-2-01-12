f4baeb7667cdb4bcc20edf2d366fc22bd2a2d1cb
TaylorEisman12 <taylor@theeismans.com>
2012-01-13 20:59:11 -0600

    added to output the current inventory.

--- Eisman_Taylor_01:12.js
+++ Eisman_Taylor_01:12.js
@@ -24,5 +24,9 @@ if ( restock == true ){
 
 };
 
-
-
+console.log( worker + " counted " + suckers + " suckers left.");
+console.log( worker + " counted " + gum + " pieces of gum left.");
+console.log( worker + " counted " + gummyBears + " gummy bears left.");
+console.log( worker + " counted " + jollyRanchers[1] + " cherry jolly ranchers left.");
+console.log( worker + " counted " + jollyRanchers[3] + " grape jolly ranchers left.");
+console.log( worker + " counted " + jollyRanchers[5] + " apple jolly ranchers left.");
