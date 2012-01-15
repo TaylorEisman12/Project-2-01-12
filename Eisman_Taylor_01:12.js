aa4ded088ba9b9e6b48d1cd3134db09fe741e1bd
TaylorEisman12 <taylor@theeismans.com>
2012-01-14 21:06:16 -0600

    Update Eisman_Taylor_01:12.js

--- Eisman_Taylor_01:12.js
+++ Eisman_Taylor_01:12.js
@@ -54,4 +54,6 @@ var workStatus = function (manager,maximumInventory){
 };
 
 // Array Function
-
+var inventoryChart = function (candy,currrentInventory){
+	console.log(currentInventory[]" .");
+};
